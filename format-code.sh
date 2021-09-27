468c0307bc81895f503fe20b42f6911c5ac683b1
Marco Gora <marco.gora@bosch.io>
2021-09-27 11:30:19 +0200

    reuse global cache folder for cashing the jar

--- format-code.sh
+++ format-code.sh
@@ -2,14 +2,14 @@
 FORMATTER_VERSION="1.11.0"
 FORMATTER="google-java-format-$FORMATTER_VERSION"
 FORMATTER_JAR="$FORMATTER-all-deps.jar"
-mkdir -p .cache
-cd .cache
-if [ ! -f $FORMATTER_JAR ]
+BASEPATH=$(dirname $0)
+mkdir -p $BASEPATH/.cache
+
+if [ ! -f $BASEPATH/.cache/$FORMATTER_JAR ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/v$FORMATTER_VERSION/$FORMATTER_JAR"
+    curl -LJOo $BASEPATH/.cache/$FORMATTER_JAR "https://github.com/google/google-java-format/releases/download/v$FORMATTER_VERSION/$FORMATTER_JAR"
     chmod 755 $FORMATTER_JAR
 fi
-cd ..
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$")
 echo $changed_java_files
